713e37212f2095e62719b3482b67397023890fd1
Ariel <arieltanami122@gmail.com>
2025-06-24 10:42:01 +0300

    Add ex2

--- 20250623.js
+++ 20250623.js
@@ -8,3 +8,17 @@ a =  a.substring(b.length)
 
 console.log(a)
 console.log(b)
+
+//ex2
+let matrix = [
+[1,'x','X',5,'X'],
+['X', 'y', 'Y', 'z', '&'],
+['A', 'א', '^', true, 5.6],
+['*', 'R', 'E', 'L', 'X'],
+['x', 3, 'B', 'X', false]
+]
+
+for(let i = 0; i< matrix.length; i++)
+  for(let j = 0; j < matrix[i].length; j++)
+    if((matrix[i][j]) === 'X')
+      console.log(`(${i + 1}, ${j + 1})`)
